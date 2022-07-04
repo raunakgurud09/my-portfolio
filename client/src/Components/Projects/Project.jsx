220469a674113c19ead08b78fb74b30cc93a5cf4
Raunak <raunakgurud2002@gmail.com>
2022-07-04 18:02:42 +0530

    implemented auto Home project fill (first 2)

--- client/src/Components/Projects/Project.jsx
+++ client/src/Components/Projects/Project.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './project.css'
 
-function Project() {
+function Project(props) {
   return (
     <div className="Project__container">
-    <a href="https://source.unsplash.com/random/500X500">
-      <img src="https://source.unsplash.com/random/50X50" alt="from unsplash" />
+    <a href="">
+      <img src={props.image} alt="from unsplash" />
     </a>
     <div className="Project__text-container">
-      <h3>This is title</h3>
+      <h3>{props.title}</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, rem!
+        {props.description}
       </p>
     </div>
   </div>
--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,20 +1,31 @@
-import './Projects.css'
-import Project from './Project'
+import "./Projects.css";
+import Project from "./Project";
+import { Projects as projectData } from "../../Data/Static/Projects";
 
 function Projects() {
   return (
-    <div className='projectHome container'>
+    <div className="projectHome container">
       <h2>Projects</h2>
       <div className="projectHome__container">
-        <Project />
-        <Project />
-        <div className="projectHome__card">
-
-        </div>
+        {projectData?.map((project, index) => {
+          if (index < 2) {
+            return (
+              <Project
+                key={index}
+                image={project.imageURI}
+                title={project.ProjectTitle}
+                description={project.ProjectSummary}
+              />
+            );
+          }else{
+            return;
+          }
+        })}
       </div>
       <button>See all</button>
     </div>
-  )
+  );
 }
 
-export default Projects
+
+export default Projects;
