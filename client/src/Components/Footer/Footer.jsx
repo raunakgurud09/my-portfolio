3e9ef7491ee926c1ea6a9fa3c91195669ea8fb3d
Raunak <raunakgurud2002@gmail.com>
2022-07-06 20:21:38 +0530

    betterprojects page with github link

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css";
-import download from './Resume.pdf'
+import download from "../../Assets/Resume.pdf";
 
 function Footer() {
   return (
@@ -13,9 +13,21 @@ function Footer() {
         <div className="up-right">
           {/* <Redirect style={{color:"var(--lightTint)" ,textDecoration:"none"}} from="/" to="/projects"><p>My work</p></Redirect> */}
           {/* <Link style={{color:"var(--lightTint)" ,textDecoration:"none"}} target="_blank" to="https://www.google.com"><p>My resume</p></Link> */}
-          <a style={{color:"var(--lightTint)" ,textDecoration:"none"}} target="_blanck" href="/projects"><p>My work</p></a>
-          <a style={{color:"var(--lightTint)" ,textDecoration:"none"}}  target="_blanck" href={download} download="Resume.pdf"><p>My resume</p></a>
-          
+          <a
+            style={{ color: "var(--lightTint)", textDecoration: "none" }}
+            target="_blanck"
+            href="/projects"
+          >
+            <p>My work</p>
+          </a>
+          <a
+            style={{ color: "var(--lightTint)", textDecoration: "none" }}
+            target="_blanck"
+            href={download}
+            download="Resume.pdf"
+          >
+            <p>My resume</p>
+          </a>
         </div>
       </div>
       <div className="footer__container-line"></div>
@@ -103,16 +115,15 @@ function Footer() {
           </a>
           <a target="_black" href="https://github.com/raunakgurud09">
             <svg
-              width="64px"
-              height="64px"
+              width="64"
+              height="64"
               viewBox="0 0 64 64"
-              id="i-github"
+              fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                strokeWidth="0"
-                fill="currentColor"
-                d="M32 0 C14 0 0 14 0 32 0 53 19 62 22 62 24 62 24 61 24 60 L24 55 C17 57 14 53 13 50 13 50 13 49 11 47 10 46 6 44 10 44 13 44 15 48 15 48 18 52 22 51 24 50 24 48 26 46 26 46 18 45 12 42 12 31 12 27 13 24 15 22 15 22 13 18 15 13 15 13 20 13 24 17 27 15 37 15 40 17 44 13 49 13 49 13 51 20 49 22 49 22 51 24 52 27 52 31 52 42 45 45 38 46 39 47 40 49 40 52 L40 60 C40 61 40 62 42 62 45 62 64 53 64 32 64 14 50 0 32 0 Z"
+                d="M32 0C14 0 0 14 0 32C0 53 19 62 22 62C24 62 24 61 24 60V55C17 57 14 53 13 50C13 50 13 49 11 47C10 46 6 44 10 44C13 44 15 48 15 48C18 52 22 51 24 50C24 48 26 46 26 46C18 45 12 42 12 31C12 27 13 24 15 22C15 22 13 18 15 13C15 13 20 13 24 17C27 15 37 15 40 17C44 13 49 13 49 13C51 20 49 22 49 22C51 24 52 27 52 31C52 42 45 45 38 46C39 47 40 49 40 52V60C40 61 40 62 42 62C45 62 64 53 64 32C64 14 50 0 32 0Z"
+                fill="black"
               />
             </svg>
           </a>
